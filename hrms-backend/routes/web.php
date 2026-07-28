67fea5d43d352c1fd0e3bbfa9c7253d89b7e7d0a
turja <UpantaBaidya@phoenix-lab.io>
2026-07-28 18:22:02 +0800

    added temp migration route

--- hrms-backend/routes/web.php
+++ hrms-backend/routes/web.php
@@ -1,7 +1,21 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/run-migrations', function () {
+    try {
+        Artisan::call('migrate:fresh', [
+            '--seed' => true,
+            '--force' => true,
+        ]);
+
+        return 'Database migrated and seeded successfully!';
+    } catch (\Exception $e) {
+        return 'Error: '.$e->getMessage();
+    }
 });
